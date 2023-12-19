ad401524ebb05ce67d0973de5ae206d924adfb0e
LucieneFreitas <luciene_freitas@yahoo.com.br>
2023-12-19 07:46:45 -0300

    aplicação next

---
+++ notas/src/data/Students.tsx
@@ -0,0 +1,56 @@
+const students = [
+  {
+    id: 1,
+    active: true,
+    name: 'Christian Buehner',
+    email: 'chris_buenergy@gmail.com',
+    avatar: 'https://images.unsplash.com/photo-1568602471122-7832951cc4c5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    grade1: 7.3,
+    grade2: 8.1
+  },
+  {
+    id: 2,
+    active: true,
+    name: 'Jonas Kakaroto',
+    email: 'jonas.k17@hotmail.com',
+    avatar: 'https://images.unsplash.com/photo-1557862921-37829c790f19?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    grade1: 2.9,
+    grade2: 8.7
+  },
+  {
+    id: 3,
+    active: false,
+    name: 'Irene Strong',
+    email: 'strong@yahoo.com.br',
+    avatar: 'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    grade1: 8.3,
+    grade2: 6.4
+  },
+  {
+    id: 4,
+    active: true,
+    name: 'Zuflano Caxias',
+    email: 'zuflano.caxias@escola.com.br',
+    avatar: 'https://images.unsplash.com/photo-1601576084861-5de423553c0f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fHJvc3RvfGVufDB8MHwwfHx8Mg%3D%3D&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    grade1: 9,
+    grade2: 8.1
+},
+{
+    id: 5,
+    active: true,
+    name: 'Turano Pires',
+    email: 'turano.pires@escola.com.br',
+    avatar: 'https://images.unsplash.com/photo-1564564244660-5d73c057f2d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDN8fHJvc3RvfGVufDB8MHwwfHx8Mg%3D%3D&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    grade1: 10,
+    grade2: 9.7
+},
+{
+    id: 6,
+    active: false,
+    name: 'Voltano Augusto',
+    email: 'voltano.augusto@escola.com.br',
+    avatar: 'https://images.unsplash.com/photo-1611689037241-d8dfe4280f2e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzN8fHJvc3RvfGVufDB8MHwwfHx8Mg%3D%3D&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    grade1: 5.5,
+    grade2: 6.2
+}
+]
